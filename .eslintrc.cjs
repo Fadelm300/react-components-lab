1d862284155dc5a798cae8e92bc3d935bfcb53d2
Fadem300 <fadel.m200@gmail.com>
2024-08-06 11:03:29 +0300

    init commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,13 +12,6 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-  rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
